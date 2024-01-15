75b774d5423ae73a1e7e025fa776b67807cb4701
Noam Gaash <noam.gaash@applitools.com>
2024-01-15 21:28:49 +0200

    refactor: auto indent

--- main.cpp
+++ main.cpp
@@ -9,52 +9,66 @@
 
 using namespace std;
 
-int dp_solution(Data& data) {
+int dp_solution(Data &data, bool verbose = false)
+{
     int n = data.jobs.size();
     int P = 0;
-    for(const auto& job : data.jobs) {
+    for (const auto &job : data.jobs)
+    {
         P += job.processing_time;
     }
 
     // initialize dp table
-    int** f = new int*[n+1];
-    for(int i = 0; i <= n; i++) {
-        f[i] = new int[P+1];
-        for(int j = 0; j <= P; j++) {
+    int **f = new int *[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        f[i] = new int[P + 1];
+        for (int j = 0; j <= P; j++)
+        {
             f[i][j] = 0;
         }
     }
 
-    for(int j=n-2; j>=-1; j--) {
-        for(int l=P; l>=0; l--) {
-            const auto& p = data.jobs[j+1].processing_time;
-            const auto& d = data.jobs[j+1].due_to;
+    for (int j = n - 2; j >= -1; j--)
+    {
+        for (int l = P; l >= 0; l--)
+        {
+            const auto &p = data.jobs[j + 1].processing_time;
+            const auto &d = data.jobs[j + 1].due_to;
 
-            if(d >= P - l) {
-                f[j+1][l] = p + f[j+2][l];
-            } else {
+            if (d >= P - l)
+            {
+                f[j + 1][l] = p + f[j + 2][l];
+            }
+            else
+            {
                 const auto &s = (P - l - p); // start time of job j+1 if it added to the late jobs
-                f[j+1][l] = min(
-                    p + f[j+2][l],
+                f[j + 1][l] = min(
+                    p + f[j + 2][l],
                     min(
                         max(0, d - s),
-                        p
-                    ) + f[j+2][l+p]
-                );
+                        p) +
+                        f[j + 2][l + p]);
             }
         }
     }
 
     // print dp table
-    for(int i = 0; i < n+1; i++) {
-        for(int j = 0; j <= P; j++) {
-            cout << f[i][j] << "\t";
+    if (verbose)
+    {
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j <= P; j++)
+            {
+                cout << f[i][j] << "\t";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 
     const auto result = f[0][0];
-    for(int i = 0; i < n+1; i++) {
+    for (int i = 0; i < n + 1; i++)
+    {
         delete[] f[i];
     }
     delete[] f;
@@ -62,24 +76,26 @@ int dp_solution(Data& data) {
     return result;
 }
 
-void process_example_file() {
+void process_example_file()
+{
     string filename = "input.csv";
     Data d(readCSV(filename));
 
     d.sort_by_descending_due_to();
     cout << "Cost before algo: " << d.cost(d.jobs) << endl;
     double result = 0;
-    double time = time_measure([&d, &result]() {
+    double time = time_measure([&d, &result]()
+                               {
         // d.jobs = naive_solution(d, d.jobs.size());
-        result = dp_solution(d);
-    });
+        result = dp_solution(d); });
 
     cout << "Time: " << time << endl;
     cout << "Cost after algo: " << result << endl;
     cout << d << endl;
 }
 
-void process_generated_data() {
+void process_generated_data()
+{
     int njobs = 7;
     int p_max = 10;
     float alpha = 1;
@@ -88,14 +104,14 @@ void process_generated_data() {
     d.sort_by_descending_due_to();
     cout << "Cost before algo: " << d.cost(d.jobs) << endl;
 
-    time_measure([&d]() {
-        d.jobs = naive_solution(d, d.jobs.size());
-    });
+    time_measure([&d]()
+                 { d.jobs = naive_solution(d, d.jobs.size()); });
 
     cout << "Cost after algo: " << d.cost(d.jobs) << endl;
 }
 
-int main() {
+int main()
+{
     process_example_file();
     // process_generated_data();
 
